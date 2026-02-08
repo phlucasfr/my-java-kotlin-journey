543cc59fae803c6871384bc731b1265bce5decb1
phlucasfr <phlucasfr@gmail.com>
2026-02-08 10:51:44 -0300

    feat: introduce Java primitive types and variable examples

---
+++ java-stdy/src/primitivos/Primitivos.java
@@ -0,0 +1,23 @@
+package primitivos;
+
+public class Primitivos {
+    public static void main(String[] args) {
+        byte idade = 32;
+        short ano = 2026;
+        int populacaoCidade = 500000;
+        long populacaoMundial = 7800000000L;
+        float altura = 1.75f;
+        double salario = 4500.50;
+        boolean estudante = true;
+        char inicialNome = 'A';
+
+        System.out.println("Idade: " + idade);
+        System.out.println("Ano: " + ano);
+        System.out.println("População da cidade: " + populacaoCidade);
+        System.out.println("População mundial: " + populacaoMundial);
+        System.out.println("Altura: " + altura);
+        System.out.println("Salário: " + salario);
+        System.out.println("É estudante? " + estudante);
+        System.out.println("Inicial do nome: " + inicialNome);
+    }
+}
--- java-stdy/src/variaveis/Variaveis.java
+++ java-stdy/src/variaveis/Variaveis.java
@@ -1,6 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+package variaveis;
+
+public class Variaveis {
     public static void main(String[] args) {
         int horasDeSono = 2;
 
