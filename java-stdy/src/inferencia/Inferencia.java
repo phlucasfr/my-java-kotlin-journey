3b2091f65581e08960c71864365e0331b53156b5
phlucasfr <phlucasfr@gmail.com>
2026-02-10 19:34:37 -0300

    feat: implementar exemplos de inferência de tipos

---
+++ java-stdy/src/inferencia/Inferencia.java
@@ -0,0 +1,12 @@
+package inferencia;
+
+public class Inferencia {
+    public static void main(String[] args) {
+        var num = 10; // aqui vai inferir como int
+        // var usuario = new Usuario(); aqui vai inferir o tipo Usuario
+
+        // limitações do var
+        // variaveis locais
+        // valores nulos
+    }
+}
